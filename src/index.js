748b1e1d5deee689c84d49aaa002761b650e7c10
Suzi <lisuzanne94@gmail.com>
2021-10-08 15:46:22 -0400

    Successfully rendered all champions on index page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 const ChampData = require('./scripts/get_champ_data.js')
-const IndexPage = require('./views/index_page')
+import IndexPage from './views/index_page'
 
 document.addEventListener("DOMContentLoaded", () => {
     const htmlEl = document.querySelector("body");
-    const indexPage = new IndexPage (htmlEl)
+    const index = new IndexPage (htmlEl)
 })
-
-// console.log(champData);
-// console.log(champData.data.Ahri.spells)
--- src/views/index_page.js
+++ src/views/index_page.js
@@ -3,18 +3,36 @@ const ChampData = require('../scripts/get_champ_data.js')
 class IndexPage {
     constructor(htmlEl) {
         this.htmlEl = htmlEl;
-        this.showPortrait("Ahri");
+        this.renderAllPortraits();
+        console.log("inside constructor")
+        // this.showPortrait("Aatrox");
     }
+    // showing single portrait
+        // showPortrait (champName) {
+        //     const champNameHeader = document.getElementById("champ-name");
+        //     const champPortrait = document.getElementById("champ-portrait");
 
-    showPortrait (champName) {
-        const champNameHeader = document.getElementById("champ-name");
-        const champPortrait = document.getElementById("champ-portrait");
+        //     champNameHeader.innerText = champName;
+        //     champPortrait.setAttribute("src", ChampData.default.getChampPortraitURL(champName));
+        // }
+    
+    // all portraits
+        renderAllPortraits () {
+            const ul = document.querySelector(".champs-list")
+            const allChampNames = ChampData.default.getAllNames();
 
-        champNameHeader.innerText = champName;
-        champPortrait.setAttribute("src", ChampData.default.getChampPortraitURL(champName));
-    }
+            allChampNames.forEach((champName) => {
+                const li = document.createElement("li");
+                const h2 = document.createElement("h2");
+                const img = document.createElement("img");
+
+                h2.innerText = champName;
+                img.setAttribute("src", ChampData.default.getChampPortraitURL(champName));
+                li.append(h2, img);
+                ul.append(li);
+            })
+        }
 }
 
-
-module.exports = IndexPage;
+export default IndexPage
 // console.log("inside index_page.js")
