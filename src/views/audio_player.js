222db080f209c599eda8fff6bd181484ac5497e5
Suzi <lisuzanne94@gmail.com>
2021-11-30 15:39:19 -0500

    add ability to toggle play pause bg audio

--- src/views/audio_player.js
+++ src/views/audio_player.js
@@ -1,6 +1,7 @@
 class Audio {
     constructor() {
         const audio = document.getElementById("bg-audio");
+        document.getElementById("play-btn").style.display = "none";
         this.audio = audio;
         this.configAudioVol();
         this.toggleAudio();
@@ -11,13 +12,23 @@ class Audio {
     }
 
     toggleAudio() {
-        const playBtn = document.getElementById("play-btn");
-        playBtn.addEventListener("click", () => {
-        if (this.audio.paused) {
+        const play = document.getElementById("play-btn");
+        const pause = document.getElementById("pause-btn");
+
+        play.addEventListener("click", () => {
             this.audio.play();
-        } else {
+            play.style.display = (play.style.display == "none") ? "inline-block" : "none";
+            if (play.style.display == "none") {
+                pause.style.display = "inline-block";
+            }
+        })
+
+        pause.addEventListener("click", () => {
             this.audio.pause();
-        }
+            pause.style.display = (pause.style.display == "none") ? "inline-block" : "none";
+            if (pause.style.display == "none") {
+                play.style.display = "inline-block";
+            }
         })
     }
 }
